cbf66cba328f3b0e017e03683b2e0224a5e05a64
Sergey <segaoff@icloud.com>
2019-02-08 22:46:41 +0300

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,12 @@
+dictionary = ['Mouth', 'Game', 'Python', 'Number', 'Farther', 'Son', 'Mother', 'Pepsi']
+
+def choice_title() 
+     title = random.choice(dictionary)
+     return title
+
+def users()
+     user = input('Write your letter')
+     return user
+def 
+
+         
